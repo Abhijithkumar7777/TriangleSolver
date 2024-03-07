cdffd4a7ea4c0998004f827c98c815275579e849
Abhijith Kumar <abhijithkumar777@gmail.com>
2024-03-06 20:05:35 -0500

    first commit with test cases

---
+++ TestTriangle/Class1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+
+namespace TestTriangle
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+   public void TriangleTestEquilateral()
+
+    {
+        
+        //Arrange
+        int firstSide=60;
+        int secondSide = 60;
+        int thirdSide = 60;
+
+        string expected = "Equilateral triangle";
+
+        //Act
+        string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+        //Assert
+        Assert.AreEqual(expected,actual);
+
+    }
+}
+
+ }
