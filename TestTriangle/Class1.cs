16079d5295ece0c36b59f15efc22005c003b84c3
Abhijith Kumar <abhijithkumar777@gmail.com>
2024-03-06 20:27:29 -0500

    Updated code with isosceles triangle testing

--- TestTriangle/Class1.cs
+++ TestTriangle/Class1.cs
@@ -13,8 +13,7 @@ namespace TestTriangle
     {
         [Test]
    public void TriangleTestEquilateral()
-
-    {
+       {
         
         //Arrange
         int firstSide=60;
@@ -29,7 +28,58 @@ namespace TestTriangle
         //Assert
         Assert.AreEqual(expected,actual);
 
-    }
-}
+       }
 
- }
+        [Test]
+        public void TriangleTestIsosceles1()
+        {
+            //Arrange
+            int firstSide = 30;
+            int secondSide = 30;
+            int thirdSide = 40;
+
+            string expected = "Isosceles triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TriangleTestIsosceles2()
+        {
+            //Arrange
+            int firstSide = 40;
+            int secondSide = 30;
+            int thirdSide = 30;
+
+            string expected = "Isosceles triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TriangleTestIsosceles3()
+        {
+            //Arrange
+            int firstSide = 30;
+            int secondSide = 40;
+            int thirdSide = 30;
+
+            string expected = "Isosceles triangle";
+
+            //Act
+            string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
